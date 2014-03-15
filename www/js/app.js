d1024335182c7d9f8c2c885f70d9c1059a087259
Wahid Hasyim <wahid.golek@gmail.com>
2014-03-15 10:55:04 +0700

    Commit ke tiga
    Mengubah rumus

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // handle bila calcBtn diklik maka hitung berat ideal
     $('#calcBtn').click(function() {
         var heightCm = parseFloat($('#heightInput').val());
-        var idealWeightKg = (heightCm - 100) * 0.9;
+        var idealWeightKg = (heightCm - 110) - ((heightCm - 110) * 0.1);
         $('#idealWeightBox').val(idealWeightKg + ' kg');
     });
     
